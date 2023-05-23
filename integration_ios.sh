13b3f36b9ccf9c4f76b1f9018ec58e63b23ea646
Matteo Bertamini <matteo@bertamini.net>
2023-05-24 01:19:48 +0200

    test signing certificate working

--- integration_ios.sh
+++ integration_ios.sh
@@ -4,14 +4,14 @@ dev_target="15.7"
 
 patrol build ios --target integration_test/example_test.dart --release --verbose
 
-pushd $product
-mv "Runner_iphoneos16.4-arm64.xctestrun" "Runner_iphoneos$dev_target-arm64.xctestrun"
-zip -r "ios_tests.zip" "Release-iphoneos" "Runner_iphoneos$dev_target-arm64.xctestrun"
-popd
-
-gcloud firebase test ios run --test "build/ios_integ/Build/Products/ios_tests.zip" \
-  --device model=iphone13pro,version=$dev_target,locale=en_US,orientation=portrait \
-  --xcode-version=14.3 \
-  --timeout 3m \
-  --results-bucket=gs://gmadrid-natacion-f5fcd.appspot.com \
-  --results-dir=tests/firebase
+#pushd $product
+#mv "Runner_iphoneos16.4-arm64.xctestrun" "Runner_iphoneos$dev_target-arm64.xctestrun"
+#zip -r "ios_tests.zip" "Release-iphoneos" "Runner_iphoneos$dev_target-arm64.xctestrun"
+#popd
+#
+#gcloud firebase test ios run --test "build/ios_integ/Build/Products/ios_tests.zip" \
+#  --device model=iphone13pro,version=$dev_target,locale=en_US,orientation=portrait \
+#  --xcode-version=14.3 \
+#  --timeout 3m \
+#  --results-bucket=gs://gmadrid-natacion-f5fcd.appspot.com \
+#  --results-dir=tests/firebase
