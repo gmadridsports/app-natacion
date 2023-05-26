afb6a487ade66e6ff6ff1d1e931ef1c20850cf61
Matteo Bertamini <matteo@bertamini.net>
2023-05-26 17:16:03 +0200

    pre-commit hook to generate release

--- integration_ios.sh
+++ integration_ios.sh
@@ -2,12 +2,12 @@ output="../build/ios_integ"
 product="build/ios_integ/Build/Products"
 dev_target="15.7"
 
-patrol build ios --target integration_test/example_test.dart --release
-
-pushd $product
-mv Runner_iphoneos*-arm64.xctestrun "Runner_iphoneos$dev_target-arm64.xctestrun"
-zip -r "ios_tests.zip" "Release-iphoneos" "Runner_iphoneos$dev_target-arm64.xctestrun"
-popd
+#patrol build ios --target integration_test/example_test.dart --release
+#
+#pushd $product
+#mv Runner_iphoneos*-arm64.xctestrun "Runner_iphoneos$dev_target-arm64.xctestrun"
+#zip -r "ios_tests.zip" "Release-iphoneos" "Runner_iphoneos$dev_target-arm64.xctestrun"
+#popd
 
 gcloud firebase test ios run --test "build/ios_integ/Build/Products/ios_tests.zip" \
   --device model=iphone13pro,version=$dev_target,locale=en_US,orientation=portrait \
