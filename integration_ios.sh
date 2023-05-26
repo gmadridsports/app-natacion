2f6f63cf933421565c546516e2d42066c95f4542
Matteo Bertamini <matteo@bertamini.net>
2023-05-26 17:47:14 +0200

    pre-commit hook to generate release

--- integration_ios.sh
+++ integration_ios.sh
@@ -2,13 +2,17 @@ output="../build/ios_integ"
 product="build/ios_integ/Build/Products"
 dev_target="15.7"
 
-#patrol build ios --target integration_test/example_test.dart --release
-#
-#pushd $product
-#mv Runner_iphoneos*-arm64.xctestrun "Runner_iphoneos$dev_target-arm64.xctestrun"
-#zip -r "ios_tests.zip" "Release-iphoneos" "Runner_iphoneos$dev_target-arm64.xctestrun"
-#popd
+committed_files=$(git show --pretty="" --name-only)
+if [ -z $(echo "$committed_files" | grep -e "^build\/ios_integ\/Build\/Products\/ios_tests\.zip$") ]; then
+  echo "Last integration test build not found. Building...";
 
+  patrol build ios --target integration_test/example_test.dart --release
+
+  pushd $product
+  mv Runner_iphoneos*-arm64.xctestrun "Runner_iphoneos$dev_target-arm64.xctestrun"
+  zip -r "ios_tests.zip" "Release-iphoneos" "Runner_iphoneos$dev_target-arm64.xctestrun"
+  popd
+fi
 
 gcloud firebase test ios run --test "build/ios_integ/Build/Products/ios_tests.zip" \
   --device model=iphone13pro,version=$dev_target,locale=en_US,orientation=portrait \
