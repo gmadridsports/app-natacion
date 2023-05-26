be2ba0c17c05e23534d93838e70cc4439e8f77dc
Matteo Bertamini <matteo@bertamini.net>
2023-05-26 17:21:43 +0200

    pre-commit hook to generate release

--- integration_ios.sh
+++ integration_ios.sh
@@ -9,6 +9,7 @@ dev_target="15.7"
 #zip -r "ios_tests.zip" "Release-iphoneos" "Runner_iphoneos$dev_target-arm64.xctestrun"
 #popd
 
+
 gcloud firebase test ios run --test "build/ios_integ/Build/Products/ios_tests.zip" \
   --device model=iphone13pro,version=$dev_target,locale=en_US,orientation=portrait \
   --xcode-version=14.3 \
