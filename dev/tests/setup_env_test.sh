ea0dab6e225fbc18aff036339c1ca83d4f543234
Matteo Bertamini <matteo@bertamini.net>
2023-08-07 21:23:54 +0200

    fixed test-artifacts

--- dev/tests/setup_env_test.sh
+++ dev/tests/setup_env_test.sh
@@ -3,7 +3,7 @@
 # todo auth-login continue here
 # prompt the user for the path of the file and store into the file located into dev/tests/env/ssh-user-src-path
 echo "Remote scp test artifacts path:"
-echo "i.e. mbertamini@dc991f7.online-server.cloud:/home/mbertamini/gmadrid-natacion/test-build/"
+echo "i.e. mbertamini@dc991f7.online-server.cloud:/home/mbertamini/gmadrid-natacion/test-artifacts/"
 echo ""
 echo "Remote scp path: \c"
 read -r SSH_USER_SRC_PATH
--- dev/tests/test_remote_sync.sh
+++ dev/tests/test_remote_sync.sh
@@ -73,7 +73,7 @@ if [ "$1" = "push" ]; then
 
   rsync -rahP -e "ssh -i '${SSH_PRIVATE_KEY_PATH}'" ${LOCAL_RSYNCED_PUSH_BASEDIR}/ ${SSH_USER_SRC_PATH}/ || exit 1
 
-  echo "Pushing the latest test artifact info to the remote one..."
+  echo "Syncing the latest generated artifact_files_latest_build.info"
   rsync -ahP -e "ssh -i '${SSH_PRIVATE_KEY_PATH}'" dev/tests/artifact_files_latest_build.info ${SSH_USER_SRC_PATH_BRANCH}/artifact_files_latest_build.info || exit 1
 
   echo "Cleaning up tmp push dir..."
