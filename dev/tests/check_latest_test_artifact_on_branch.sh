01a87a8a715173b01c908602d2a49db8f644c357
Matteo Bertamini <matteo@bertamini.net>
2023-08-08 19:02:02 +0200

    debugging

--- dev/tests/check_latest_test_artifact_on_branch.sh
+++ dev/tests/check_latest_test_artifact_on_branch.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env /bin/bash
 
-BRANCH_NAME=${GITHUB_HEAD_REF:-$(git branch --show-current)}
-git pull origin ${BRANCH_NAME} || exit 1;
-git diff --word-diff=porcelain origin/${BRANCH_NAME}~1 origin/${BRANCH_NAME} -- dev/tests/artifact_build_timestamp.info  > /tmp/timestamp-diff.txt || exit 2;
-#cat /tmp/timestamp-diff.txt
+if [[ ! -z "$CI" ]]; then
+  BRANCH_NAME=${GITHUB_HEAD_REF:-$(git branch --show-current)}
+  git pull origin ${BRANCH_NAME} || exit 1;
+  git diff --word-diff=porcelain origin/${BRANCH_NAME}~1 origin/${BRANCH_NAME} -- dev/tests/artifact_build_timestamp.info  > /tmp/timestamp-diff.txt || exit 2;
+else
+  git diff --word-diff=porcelain HEAD~1 -- dev/tests/artifact_build_timestamp.info  > /tmp/timestamp-diff.txt || exit 2;
+fi
+
 new_timestamp=$(cat /tmp/timestamp-diff.txt | grep --extended-regexp "^\+[0-9]{19}")
 old_timestamp=$(cat /tmp/timestamp-diff.txt | grep --extended-regexp "^-[0-9]{19}")
 new_timestamp="${new_timestamp:1}"
