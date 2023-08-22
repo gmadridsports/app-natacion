d4c84789e2e3c8f844dfa2ab9b2ed60608044d97
Matteo Bertamini <matteo@bertamini.net>
2023-08-23 01:34:46 +0200

    backend has the right protection rules, according to the new authentication WIP: profiles has been tested, buckets must be TDDed accordingly. DB schema migration to be updated

--- supabase/tests/database/02-profiles.test.sql
+++ supabase/tests/database/02-profiles.test.sql
@@ -1,17 +1,17 @@
 -- begin the transaction, this will allow you to rollback any changes made during the test
 BEGIN;
 
-select plan(3);
+select plan(9);
 
 select tests.clear_authentication();
 
 select tests.rls_enabled('public');
 
--- select policies_are(
---                'public',
---                'profiles',
---                ARRAY['Can only access its own profile']
---            );
+select policies_are(
+               'public',
+               'profiles',
+               ARRAY['authenticated users can only read their own profile']
+           );
 
 SELECT tests.create_supabase_user('authenticated_non_member_user', 'authenticated@gmadridnatacion.bertamini.net');
 SELECT tests.create_supabase_user('authenticated_non_member_user_2', 'authenticated2@gmadridnatacion.bertamini.net');
@@ -30,6 +30,32 @@ select results_eq(
                $$VALUES((tests.get_supabase_user('authenticated_non_member_user')->'id')::text) $$,
                'Authenticated user can only access its own profile'
            );
+
+select results_eq(
+               'SELECT concat(''"'', id::text, ''"'') FROM profiles;',
+               $$VALUES((tests.get_supabase_user('authenticated_non_member_user')->'id')::text) $$,
+               'Authenticated user can only access its own profile'
+           );
+
+select throws_ok(
+               $$ insert into public.profiles (id, membership_level) VALUES (uuid_generate_v4(), 'socio-23-24'); $$,
+               'new row violates row-level security policy for table "profiles"'
+           );
+
+select is_empty(
+                $$ UPDATE public.profiles SET membership_level = 'new-membership' WHERE id = auth.uid() returning 'this should not be returned' $$,
+               'Authenticated user cannot update its own profile'
+           );
+
+select is_empty(
+               $$ UPDATE public.profiles SET membership_level = 'new-membership' returning 'this should not be returned' $$,
+               'Authenticated user cannot update other peoples profile'
+           );
+
+SELECT is_empty(
+            $$ delete from public.profiles returning 1 $$,
+            'cannot delete any profile'
+        );
 
 select * from finish();
 
