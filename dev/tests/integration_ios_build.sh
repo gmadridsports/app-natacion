ba51ae54816df2885cb26c474bf64548dcd659e3
Matteo Bertamini <matteo@bertamini.net>
2023-09-01 13:49:50 +0200

    Associate domain with the app both android and iOS. disabled android to test the iOS building and testing process on github actions p

--- dev/tests/integration_ios_build.sh
+++ dev/tests/integration_ios_build.sh
@@ -5,6 +5,8 @@ product="build/ios_integ/Build/Products"
 dev_target="15.7"
 
 echo "Building the integration tests for iOS 🍏"
+echo   "debug"
+echo ${SUPABASE_ADMIN_TEST_PASSWORD};
 patrol build ios --target integration_test/main_test.dart --release --dart-define="ENV=test" --dart-define="SUPABASE_ADMIN_TEST_PASSWORD=${SUPABASE_ADMIN_TEST_PASSWORD}"
 
 pushd $product
