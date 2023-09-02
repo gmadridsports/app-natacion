f54b606562c0c5c7674bcfb0343f1e0ca51ddb5f
Matteo Bertamini <matteo@bertamini.net>
2023-09-02 02:06:34 +0200

    Email as valueobject for the login screen

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -6,6 +6,7 @@ import 'package:gmadrid_natacion/models/RunningMode.dart';
 import 'package:gmadrid_natacion/screens/training-week/training-week.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+import '../../models/Email.dart';
 import '../NamedRouteScreen.dart';
 
 class Login extends StatefulWidget implements NamedRouteScreen {
@@ -31,13 +32,18 @@ class _LoginState extends State<Login> {
         _isLoading = true;
       });
 
+      if (_emailController.text.isEmpty) {
+        return;
+      }
+
+      final email = Email.fromString(_emailController.text);
+
       if (RunningMode.fromEnvironment().isTestingMode()) {
         final response = await Supabase.instance.client.auth.signInWithPassword(
-            email: _emailController.text.trim(),
-            password: _passwordController.text.trim());
+            email: email.toString(), password: _passwordController.text.trim());
       } else {
         await Supabase.instance.client.auth.signInWithOtp(
-          email: _emailController.text.trim(),
+          email: email.toString(),
           emailRedirectTo: kIsWeb
               ? null
               : 'https://authgmadridnatacion.bertamini.net/login-callback',
@@ -58,7 +64,7 @@ class _LoginState extends State<Login> {
       );
     } catch (error) {
       SnackBar(
-        content: const Text('Unexpected error occurred'),
+        content: const Text('Ha ocurrido un error inesperado'),
         backgroundColor: Theme.of(context).colorScheme.error,
       );
     } finally {
@@ -87,40 +93,21 @@ class _LoginState extends State<Login> {
   @override
   void dispose() {
     _emailController.dispose();
+    _passwordController.dispose();
     _authStateSubscription.cancel();
     super.dispose();
   }
 
-  String? validateEmail(String? value) {
-    print('validateEmail');
-    const pattern = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"
-        r'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-'
-        r'\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*'
-        r'[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4]'
-        r'[0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9]'
-        r'[0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\'
-        r'x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
-    final regex = RegExp(pattern);
-
-    return value!.isNotEmpty && !regex.hasMatch(value)
-        ? 'Usa un email válido'
-        : null;
-  }
-
   String? get _errorText {
-    final text = _emailController.value.text;
-    const pattern = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"
-        r'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-'
-        r'\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*'
-        r'[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4]'
-        r'[0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9]'
-        r'[0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\'
-        r'x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
-    final regex = RegExp(pattern);
+    if (_emailController.value.text.isEmpty) return null;
 
-    return text!.isNotEmpty && !regex.hasMatch(text)
-        ? 'Usa un email válido'
-        : null;
+    try {
+      Email email = Email.fromString(_emailController.value.text);
+    } catch (error) {
+      return error.toString();
+    }
+
+    return null;
   }
 
   @override
@@ -134,15 +121,11 @@ class _LoginState extends State<Login> {
     final scaffoldElements = [
       const Image(
           image: AssetImage('assets/images/logo-mascota.png'), height: 250),
-      // const Text(
-      //   'Accede a la aplicación con tu email',
-      //   style: TextStyle(fontSize: 20),
-      //   textAlign: TextAlign.center,
-      // ),
       const SizedBox(height: 18),
       TextField(
         keyboardType: TextInputType.emailAddress,
         controller: _emailController,
+        enabled: !_isLoading,
         onSubmitted: (_) => _signIn(),
         style: const TextStyle(fontSize: 20),
         decoration: InputDecoration(
