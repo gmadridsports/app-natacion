10821d3e0f995e0e14476baa509575554aea5138
Matteo Bertamini <matteo@bertamini.net>
2024-03-17 15:35:09 +0100

    bulky commit: improve the adaptive icon for android, fix first selected event calendar date, add bulletin_board backend table

---
+++ supabase/migrations/20240314002750_bulletin-board.sql
@@ -0,0 +1,25 @@
+create table
+    public.bulletin_board
+(
+    id                        uuid                        not null,
+    publication_date          timestamp without time zone not null,
+    is_published              boolean                     not null,
+    source_id                 text                        not null,
+    origin_source             text                        not null,
+    body_message              text                        not null,
+    original_raw_data_message jsonb                       not null,
+    constraint bulletin_board_pkey primary key (id),
+    constraint bulletin_board_origin_source_source_id_key unique (origin_source, source_id)
+) tablespace pg_default;
+
+alter table "public"."bulletin_board"
+    enable row level security;
+
+create policy "authenticated member can read bulletin_board"
+    on "public"."bulletin_board"
+    as permissive
+    for select
+    to authenticated
+    using ('member' in (select membership_level
+                        from public.profiles
+                        where id = auth.uid()));
